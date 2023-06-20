cf469b857cd332e0dcbde76874873873383bf589
Bob-helloworld <57881497+Bob-helloworld@users.noreply.github.com>
2023-06-20 20:37:53 +0800

    Update MultiscaleDiffusionClusteringTest.py

--- SA_KMeans/多尺度扩散聚类/MultiscaleDiffusionClusteringTest.py
+++ SA_KMeans/多尺度扩散聚类/MultiscaleDiffusionClusteringTest.py
@@ -4,7 +4,6 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import cv2
 
 
 # 从文件中读取数据
@@ -50,21 +49,24 @@ def target_function(near_cen, dist):
 def multiscale_diffusion_clustering(data, k):
     # step 1: init. centers
     np.random.seed(2022)
-    centers = data[np.random.choice(len(data), size=k,  replace=False), :]  # 随机产生k个质心
+    centers = np.random.choice(np.arange(-5, 5, 0.1), (k, 2))  # 随机产生k个质心
     near_cen = [0]
-    old_class = near_cen
     # step 2: 点归属
     near_cen, dist = near_center(data, centers)
     # step 3：计算系统浓度
     density = target_function(near_cen, dist)
+    # 选择数据集样本点之间的最大欧式距离作为初始扩散尺度,
     d = np.argmax(dist)
+    best_centers = centers
+    best_near_cen = near_cen
+    best_dist = dist
     while d > 0.01:
         for i in range(100):  # 最多做100次迭代
-            # step 4：选择数据集样本点之间的最大欧式距离作为初始扩散尺度,生成高斯随机数
+            # step 4：生成高斯随机数
             random_number = np.random.normal(0, 1, [len(centers), 2])
             # step 5：簇的扩散
             new_centers = centers + d * random_number
-            print(f"random_number={random_number}, \ncenters={centers}, , \nnew_centers={new_centers}")
+            print(f"random_number={random_number}, \nnew_centers={new_centers} , \nbest_centers={best_centers}")
             # step 6：计算比较浓度的变化
             new_near_cen, new_dist = near_center(data, centers)
             new_density = target_function(new_near_cen, new_dist)
@@ -74,12 +76,12 @@ def multiscale_diffusion_clustering(data, k):
                 best_centers = new_centers
                 best_near_cen = new_near_cen
                 best_dist = new_dist
-        d = 0.8 * d
+        d = 0.3 * d
     return best_centers, best_near_cen, best_dist
 
 
 if __name__ == '__main__':
-    path = 'testdata.txt'
+    path = 'D:\\GitHubProject\\MachineLearningRelative\\SA_KMeans\多尺度扩散聚类\\testdata.txt'
     data = read_data(path)
     # step1 先进行一次kmeans聚类获得初始解并获得目标函数值（每个点距离自己类别中心点的距离之和）
     centers, near_cen, dist = multiscale_diffusion_clustering(data, 4)
